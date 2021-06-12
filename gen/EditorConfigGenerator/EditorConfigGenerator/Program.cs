c7a10f107f7a4e0e581c0aaedda631f81fb9f225
Dan Friedman <dan@thefriedmans.org>
2021-06-12 07:52:17 -0500

    Start generator

---
+++ gen/EditorConfigGenerator/EditorConfigGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EditorConfigGenerator
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CopySchema();
+        }
+
+        private static async Task CopySchema()
+        {
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://raw.githubusercontent.com/madskristensen/EditorConfigLanguage/master/src/Schema/EditorConfig.json");
+
+            var directory = Directory.CreateDirectory("Schema");
+            using var file = File.Create($"{directory.Name}/EditorConfig.json");
+            await response.Content.CopyToAsync(file);
+        }
+    }
+}
